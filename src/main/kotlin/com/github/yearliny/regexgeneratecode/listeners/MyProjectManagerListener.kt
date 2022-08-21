42fba90d2838d68e4b968d1a724f8d1d9585688c
GitHub Action <action@github.com>
2022-08-21 11:46:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/yearliny/regexgeneratecode/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/yearliny/regexgeneratecode/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yearliny.regexgeneratecode.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yearliny.regexgeneratecode.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/yearliny/regexgeneratecode/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yearliny/regexgeneratecode/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yearliny.regexgeneratecode.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yearliny.regexgeneratecode.MyBundle
 
 class MyApplicationService {
 
